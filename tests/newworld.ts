e010edd7bbdfab8fac1f919c91983b3cbc2e580b
Jacob Shiohira <jacobshiohira@jshiohahambp.local>
2022-05-18 18:59:08 -0700

    build(repo): initial commit

---
+++ tests/newworld.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Newworld } from "../target/types/newworld";
+
+describe("newworld", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Newworld as Program<Newworld>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
