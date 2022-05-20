0a33e69984c7dbd1a12cfc4dd8191dd4b1495d77
jshiohaha <dev@jacobshiohira.com>
2022-05-20 09:15:20 -0700

    patch: test commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  "rules": {
+    "type-enum": () => [0, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'patch', 'minor', 'major']],
+  }
 };
