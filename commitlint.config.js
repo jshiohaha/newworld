05fbf238c83d986fa48a774ccf65e666a3e33212
Restyled.io <commits@restyled.io>
2022-05-20 18:26:12 +0000

    Restyled by clang-format

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  "rules": {
-    "type-enum": () => [0, "always", ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'patch', 'minor', 'major']],
+  extends : [ "@commitlint/config-conventional" ],
+  "rules" : {
+    "type-enum" : () => [0, "always",
+                         [
+                           'feat', 'fix', 'docs', 'style', 'refactor', 'test',
+                           'revert', 'patch', 'minor', 'major'
+                         ]],
   }
 };
