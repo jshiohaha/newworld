0c53b2ebc0aa8aadfc2cb4a49774177a9c2ac20d
jshiohaha <dev@jacobshiohira.com>
2022-06-03 16:45:44 -0700

    test: fuck

--- .github/actions/update-cargo-version/script.js
+++ .github/actions/update-cargo-version/script.js
@@ -21,26 +21,30 @@ module.exports = ({ github, context, toml }, semvar) => {
   // const CARGO_TOML_PATH = "./Cargo.toml";
   console.log("CARGO_TOML_PATH: ", CARGO_TOML_PATH);
   // Test both the read and write permissions
-  fs.access(CARGO_TOML_PATH, fs.constants.R_OK | fs.constants.W_OK, (err) => {
-    console.log("semvar: ", semvar);
+  // | fs.constants.W_OK
+  fs.access(CARGO_TOML_PATH, fs.constants.R_OK, (err) => {
     console.log("\n> Checking Permission for reading and writing to file");
-    if (err) throw new Error("No read and write access");
-    // const { SHA } = process.env;
-    let tomlObj = toml.parse(fs.readFileSync(CARGO_TOML_PATH, "utf-8"));
-    console.log("tomlObj: ", tomlObj);
-    const package = tomlObj.package;
-    console.log("package: ", package);
-    if (!package) throw new Error("No package tag defined in Cargo.toml");
-    let [major, minor, patch] = package.version.split(".").map((v) => +v);
-    console.log("major: ", major);
-    console.log("minor: ", minor);
-    console.log("patch: ", patch);
-    const updatedVersion = getUpdatedVersion(semvar, { major, minor, patch });
-    console.log("updatedVersion: ", updatedVersion);
-    // update version
-    tomlObj.package.version = updatedVersion;
-    // print updated version
-    console.log("tomlObj: ", tomlObj);
+    if (err) {
+      throw new Error("No read and write access");
+    }
+    // else
+    console.log("access ok");
+
+    // let tomlObj = toml.parse(fs.readFileSync(CARGO_TOML_PATH, "utf-8"));
+    // console.log("tomlObj: ", tomlObj);
+    // const package = tomlObj.package;
+    // console.log("package: ", package);
+    // if (!package) throw new Error("No package tag defined in Cargo.toml");
+    // let [major, minor, patch] = package.version.split(".").map((v) => +v);
+    // console.log("major: ", major);
+    // console.log("minor: ", minor);
+    // console.log("patch: ", patch);
+    // const updatedVersion = getUpdatedVersion(semvar, { major, minor, patch });
+    // console.log("updatedVersion: ", updatedVersion);
+    // // update version
+    // tomlObj.package.version = updatedVersion;
+    // // print updated version
+    // console.log("tomlObj: ", tomlObj);
     // save updated version
     // fs.writeFileSync(CARGO_TOML_PATH, toml.stringify(tomlObj));
   });
