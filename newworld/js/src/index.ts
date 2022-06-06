2587b9672e94a3dd5cdbdf30b3fa261e62683163
Jacob Shiohira <jlshiohira23@gmail.com>
2022-06-06 14:55:00 -0700

    build: hopefully last comment lol (#12)

--- newworld/js/src/index.ts
+++ newworld/js/src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop bop mcbooper bop');
+    console.log('boop bop mcbooper boopity');
   }
   return a + b;
 };
