1594fcc3f0178fd6c93e0962d7945b45ed7e1b42
jshiohaha <dev@jacobshiohira.com>
2022-06-24 14:03:15 -0500

    add workflow on pr comment

---
+++ .github/actions/list-changed-files/script.js
@@ -0,0 +1,68 @@
+const getPackageAndExtension = (path) => {
+  const invalidPathDefault = ["", ""];
+
+  const pathParts = path.trim().toLowerCase().split("/");
+  if (pathParts.length === 0) return invalidPathDefault;
+  const extensionParts = pathParts[pathParts.length - 1].split(".");
+  // we expect at least 2 items in the result: ['name', 'ending']
+  if (extensionParts.length < 2) return invalidPathDefault;
+
+  // [package, extension]
+  return [pathParts[0], extensionParts[1]];
+};
+
+fetchAllChangedFiles = async (
+  github,
+  owner,
+  repo,
+  pull_number,
+  per_page = 100 // max = 100?
+) => {
+  let page = 0;
+  let files = [];
+
+  while (true) {
+    const { data } = await github.pulls.listFiles({
+      owner,
+      repo,
+      pull_number,
+      direction: "asc",
+      per_page,
+      page,
+    });
+
+    if (data.length === 0) break;
+    files = [...files, ...data.map((f) => f.filename)];
+
+    console.log(`fetched page ${page}`);
+    page += 1;
+  }
+
+  console.log(`Fetched ${files.length} files for PR ${pull_number}`);
+
+  return files;
+};
+
+module.exports = async (
+  { github, context, core },
+  pull_number,
+  target_package,
+  target_extensions
+) => {
+  const parsed_target_extensions = JSON.parse(target_extensions);
+  console.log(parsed_target_extensions);
+  const changedFiles = await fetchAllChangedFiles(
+    github,
+    context.repo.owner,
+    context.repo.repo,
+    pull_number
+  );
+
+  console.log("num changedFiles: ", changedFiles.length);
+  console.log("changedFiles: ", changedFiles);
+
+  core.exportVariable(
+    "CHANGED_FILES",
+    JSON.stringify(Array.from(Object.keys(changedFiles)))
+  );
+};
