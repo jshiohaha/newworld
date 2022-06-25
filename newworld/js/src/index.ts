3b428e0d8e06dfca8597384ffb04af91fce6408d
jshiohaha <dev@jacobshiohira.com>
2022-06-25 18:47:00 -0500

    comment

--- newworld/js/src/index.ts
+++ newworld/js/src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop bop mcbooper boopity');
+    console.log('boop bop mcbooper boopity boop');
   }
   return a + b;
 };
